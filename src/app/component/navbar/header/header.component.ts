e132aa92b4c48f09fea0bd50806546864dc83d3f
rawdasherif <rawdasherif119@gmail.com>
2019-04-13 00:13:46 +0200

    remove icon in drop downlist of cart

--- src/app/component/navbar/header/header.component.ts
+++ src/app/component/navbar/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {CounterService} from 'src/app/counter.service';
 import { ProductService } from 'src/app/product.service';
+import { IProduct } from 'src/app/models/product';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +10,7 @@ import { ProductService } from 'src/app/product.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public products =[];
   public cartItemCountarr=[];
   public cartItemCount:number ;
   public cartlist=[];
@@ -16,8 +18,10 @@ export class HeaderComponent implements OnInit {
   constructor(private _router:Router,private counter:CounterService,private _productService: ProductService) { }
 
   ngOnInit() {
-this.cartlist=this._productService.getProductFromCart()
-console.log(this.cartlist)
+    this._productService.getProducts().subscribe(
+      data=> this.products = data);
+      this.cartlist=this._productService.getProductFromCart()
+      console.log(this.cartlist)
      this.cartItemCountarr= this.counter. getcartcount()
      if( this.cartItemCountarr == null){
       this.cartItemCountarr=[];
@@ -43,6 +47,17 @@ console.log(this.cartlist)
 
  logout(){
   this._router.navigate(['login']);
+ }
+
+  removeitem(product:IProduct){
+  this.cartlist=this._productService.getProductFromCart();
+  this.cartlist.find(p=>p.ProductId==product.ProductId);
+  var index=this.cartlist.indexOf(this.cartlist.find(p=>p.ProductId==product.ProductId));
+  this.cartlist.splice(index,1);
+  this._productService.removeProductFromCart();
+  this._productService.addProductToCart(this.cartlist);
+  window.location.reload();
+
  }
 
 }
